13d0016e614a691a67c1afa028b4fc8bdcfe07c9
Shanks <shashan@gmail.com>
2015-01-31 19:39:59 -0600

    commiting test projects from macos

---
+++ Test_cpp/Test_R.R
@@ -0,0 +1,11 @@
+# TODO: Add comment
+# 
+# Author: Shanks
+###############################################################################
+
+
+a = 5
+b = 3
+vars <- list()
+vars = cbind(a,b)
+summary(vars)
